c4444ddb8900fa9330e16e1f5c099bf33d242b70
Zach Couchman <zachary.couchman@immutable.com>
2024-07-02 10:25:06 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,142 @@
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import cors from '@fastify/cors';
+import logger from './logger'
+// Initialize Fastify instance
+const fastify = Fastify();
+
+// Enable CORS with specified options for API security and flexibility
+fastify.register(cors, {
+  origin: "*", // Allow all origins
+  methods: ["GET"], // Supported HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed HTTP headers
+});
+
+fastify.get("/token/:id", async (request: FastifyRequest, reply: FastifyReply) => {
+  const tokenId = request.params["id"];
+  let bigTokenId;
+  try {
+    bigTokenId = BigInt(tokenId);
+  } catch (err) {
+    reply.status(400).send({ status: 400, message: 'id must be a number' })
+  }
+
+  if (!bigTokenId) {
+    reply.status(500).send({ wrong: 'wrong' });
+  }
+
+  const remainder = bigTokenId! % BigInt(3);
+
+  if (remainder === BigInt(2)) {
+    reply.status(200).send({
+      "id": bigTokenId!.toString(),
+      "image": "https://zacharycouchman.github.io/nft-project-metadata-immutable/tokens/token1.webp",
+      "token_id": bigTokenId!.toString(),
+      "background_color": null,
+      "animation_url": null,
+      "youtube_url": null,
+      "name": "Jeffrey",
+      "description": "Owl",
+      "external_url": null,
+      "attributes": [
+        {
+          "trait_type": "Eyesight",
+          "value": 10
+        },
+        {
+          "trait_type": "Flying",
+          "value": 8
+        },
+        {
+          "trait_type": "Nocturnal",
+          "value": true
+        },
+        {
+          "trait_type": "Peck",
+          "value": 5
+        }
+      ]
+    })
+    return;
+  }
+
+  if (remainder === BigInt(1)) {
+    reply.status(200).send({
+      "id": bigTokenId!.toString(),
+      "image": "https://zacharycouchman.github.io/nft-project-metadata-immutable/tokens/token2.webp",
+      "token_id": bigTokenId!.toString(),
+      "background_color": null,
+      "animation_url": null,
+      "youtube_url": null,
+      "name": "Mike",
+      "description": "Rooster",
+      "external_url": null,
+      "attributes": [
+        {
+          "trait_type": "Eyesight",
+          "value": 4
+        },
+        {
+          "trait_type": "Flying",
+          "value": 2
+        },
+        {
+          "trait_type": "Nocturnal",
+          "value": false
+        },
+        {
+          "trait_type": "Peck",
+          "value": 10
+        }
+      ]
+    })
+    return;
+  }
+
+  if (remainder === BigInt(0)) {
+    reply.status(200).send({
+      "id": bigTokenId!.toString(),
+      "image": "https://zacharycouchman.github.io/nft-project-metadata-immutable/tokens/token3.webp",
+      "token_id": bigTokenId!.toString(),
+      "background_color": null,
+      "animation_url": null,
+      "youtube_url": null,
+      "name": "Gary",
+      "description": "Eagle",
+      "external_url": null,
+      "attributes": [
+        {
+          "trait_type": "Eyesight",
+          "value": 10
+        },
+        {
+          "trait_type": "Flying",
+          "value": 10
+        },
+        {
+          "trait_type": "Nocturnal",
+          "value": false
+        },
+        {
+          "trait_type": "Peck",
+          "value": 7
+        }
+      ]
+    })
+    return;
+  }
+})
+
+// Start the server
+const start = async () => {
+  try {
+    await fastify.listen({
+      port: 3000
+    });
+    logger.info(`Server running...`);
+  } catch (err: any) {
+    logger.error("Error starting server:", err);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ src/logger.ts
@@ -0,0 +1,41 @@
+import winston, { transport, format } from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+
+const transportsArray: transport[] = [
+  new winston.transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      format.printf((info) => {
+        // If info.message is an object, pretty print it. Otherwise, leave it as is.
+        const message = typeof info.message === "object" ? JSON.stringify(info.message, null, 2) : info.message;
+        return `[${info.timestamp}][${info.level}]${message}`;
+      })
+    ),
+  }),
+  // ... other code
+];
+
+//If logging to file is enabled in config.ts, let's output to file, we also want to use a file per day, datePattern dictates the frequency
+transportsArray.push(
+  new DailyRotateFile({
+    filename: "logs/minting-api-backend-%DATE%.log",
+    datePattern: "YYYY-MM-DD",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+    format: format.combine(
+      format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      format.printf((info) => `${JSON.stringify({ timestamp: info.timestamp, level: info.level, message: info.message })}`)
+    ),
+  })
+);
+
+const logger = winston.createLogger({
+  level: 'debug',
+  transports: transportsArray,
+});
+
+export default logger;
